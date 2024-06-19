aec7da2f592639a8bfb03211d3f4a9d5a2b437ce
TristanRib <tristanrib_@outlook.fr>
2024-06-19 21:48:27 +0200

    fix(mock): remove uids from mock

--- tests/__mocks__/Animal.ts
+++ tests/__mocks__/Animal.ts
@@ -1,6 +1,6 @@
 import {SpecieEnum} from "../../server/enums/SpecieEnum";
 
 export class Animal {
-    constructor(public uid: string, public name: string, public age: number, public specie: SpecieEnum) {
+    constructor(public name: string, public age: number, public specie: SpecieEnum) {
     }
 }
